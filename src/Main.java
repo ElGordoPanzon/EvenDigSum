366488b4faefde67d20f40ab9f7bbb1f15ecd12d
jose jimenez <jimenjos000@warren.k12.in.us>
2024-09-23 07:43:55 -0400

    bn

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    private static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+
+
+        int sum = 0;
+
+        while (number > 0) {
+            int lastdigit = number % 10;
+            if (lastdigit % 2 == 0) {
+                sum += lastdigit;
+
+            }number /= 10;
+
+
+            // while number is greater than 0.
+            // get the last digit with mod store in variable.
+            // check if its even.  % 2
+            // if even add to sum.
+            // number divide equals 10.
+
+        } return sum;
+
+
+    }
 }
